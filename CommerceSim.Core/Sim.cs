1aca9e0200e59bb17491bff299a411fe0e4fbfa7
Jacob Foshee <jacobf@gmail.com>
2026-04-12 08:51:34 -0500

    refactor: generalize system entity initialization

--- CommerceSim.Core/Sim.cs
+++ CommerceSim.Core/Sim.cs
@@ -28,19 +28,19 @@ public class Sim
             _entities.Add(entity);
         }
 
-        // TODO: loop over systems
-        // Init commerce system with entities that have snapshot
-        var commercialEntities = entitiesWithState
-            .Where(e => e.entity is ICommercialEntity || e.initialStates.OfType<CommercialSnapshot>().Any())
-            .Select(e => (e.entity, e.initialStates.OfType<CommercialSnapshot>().Cast<CommercialSnapshot?>().FirstOrDefault()))
-            .ToArray();
-        CommercialSystem.InitEntities(commercialEntities);
-        // Init spatial system with entities that are marked as ISpatialEntity or have initial position
-        var spatialEntities = entitiesWithState
-            .Where(e => e.entity is ISpatialEntity || e.initialStates.OfType<PositionSnapshot>().Any())
-            .Select(e => (e.entity, e.initialStates.OfType<PositionSnapshot>().Cast<PositionSnapshot?>().FirstOrDefault()))
-            .ToArray();
-        SpatialSystem.InitEntities(spatialEntities);
+        // Initialize each system with matching entities
+        foreach (var system in _systems)
+        {
+            var (markerInterface, snapshotType) = GetSystemTypeInfo(system);
+            // Identify entities that either implement the marker interface
+            // or have an initial state matching the system's snapshot type
+            var matchingEntities = entitiesWithState
+                .Where(e => markerInterface.IsInstanceOfType(e.entity)
+                            || e.initialStates.Any(s => IsSnapshotForSystem(s, system.GetType())))
+                .Select(e => (e.entity, e.initialStates.FirstOrDefault(s => IsSnapshotForSystem(s, system.GetType()))))
+                .ToArray();
+            InvokeInitEntities(system, snapshotType, matchingEntities);
+        }
 
         // Init commercial sources
         var sources = entitiesWithState.Select(e => e.entity).OfType<ISource>().ToArray();
@@ -68,4 +68,36 @@ public class Sim
         if (entity is IRequire<CommercialSystem> commerceEntity)
             commerceEntity.Inject(CommercialSystem);
     }
+
+    #region Ugly Reflection for generic initialization
+    private static bool IsSnapshotForSystem(IStateSnapshot snapshot, Type systemType) =>
+        snapshot.GetType().GetInterfaces()
+            .Any(i => i.IsGenericType
+                && i.GetGenericTypeDefinition() == typeof(IStateSnapshot<>)
+                && i.GetGenericArguments()[0].IsAssignableFrom(systemType));
+
+    private static (Type markerInterface, Type snapshotType) GetSystemTypeInfo(ISystem system) =>
+        system.GetType().GetInterfaces()
+            .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(ISystem<,>))
+            .Select(i => (markerInterface: i.GetGenericArguments()[0], snapshotType: i.GetGenericArguments()[1]))
+            .First();
+
+    private static void InvokeInitEntities(ISystem system, Type snapshotType, (IEntity entity, IStateSnapshot? state)[] entities)
+    {
+        // Build array of (IEntity, TSnapshot?)
+        var nullableSnapshotType = typeof(Nullable<>).MakeGenericType(snapshotType);
+        var tupleType = typeof(ValueTuple<,>).MakeGenericType(typeof(IEntity), nullableSnapshotType);
+        var typedArray = Array.CreateInstance(tupleType, entities.Length);
+
+        for (int i = 0; i < entities.Length; i++)
+        {
+            var (entity, state) = entities[i];
+            var typedTuple = Activator.CreateInstance(tupleType, entity, state);
+            typedArray.SetValue(typedTuple, i);
+        }
+
+        var method = system.GetType().GetMethod("InitEntities")!;
+        method.Invoke(system, [typedArray]);
+    }
+    #endregion
 }
