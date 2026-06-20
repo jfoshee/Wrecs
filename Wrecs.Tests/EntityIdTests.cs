8617824b6fcd4efdc8d40e22e3252ea0c0f7f305
Jacob Foshee <jacobf@gmail.com>
2026-06-20 14:00:14 -0500

    test: improve entity ID test

--- Wrecs.Tests/EntityIdTests.cs
+++ Wrecs.Tests/EntityIdTests.cs
@@ -1,14 +1,22 @@
 namespace Wrecs.Tests;
 
+[CollectionDefinition(nameof(EntityIdTests), DisableParallelization = true)]
+public class EntityIdTestsCollection;
+
+[Collection(nameof(EntityIdTests))]
 public class EntityIdTests
 {
     [Fact(DisplayName = "Simultaneous ID generations are all unique")]
     public async Task Next_WhenCalledSimultaneously_ReturnsUniqueIds()
     {
         const int count = 512;
+        // Use a gate to prevent the tasks from starting until they are all ready
+        var gate = new TaskCompletionSource();
         var tasks = Enumerable.Range(0, count)
-                              .Select(_ => Task.Run(() => EntityId.Next()))
+                              .Select(_ => Task.Run(async () => { await gate.Task; return EntityId.Next(); }))
                               .ToArray();
+        gate.SetResult();
+
         var ids = await Task.WhenAll(tasks);
 
         ids.Should().OnlyHaveUniqueItems();
