b983e1266435f36c02039ab444b81770e2c1e04b
Jacob Foshee <jacobf@gmail.com>
2026-07-20 11:08:55 -0500

    feat(maze): create SDL 3 Window tested on Mac based on https://github.com/edwardgushchin/SDL3-CS#-examples

--- Wrecs.Game.Maze/Program.cs
+++ Wrecs.Game.Maze/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SDL3;
+
+Console.WriteLine("Hello, World!");
+
+if (!SDL.CreateWindowAndRenderer("SDL3 Create Window", 800, 600, 0, out var window, out var renderer))
+{
+    SDL.LogError(SDL.LogCategory.Application, $"Error creating window and rendering: {SDL.GetError()}");
+    return;
+}
+
+SDL.SetRenderDrawColor(renderer, 100, 149, 237, 255);
+
+var loop = true;
+
+while (loop)
+{
+    while (SDL.PollEvent(out var e))
+    {
+        if ((SDL.EventType)e.Type == SDL.EventType.Quit)
+        {
+            loop = false;
+        }
+    }
+
+    SDL.RenderClear(renderer);
+    SDL.RenderPresent(renderer);
+}
+
+SDL.DestroyRenderer(renderer);
+SDL.DestroyWindow(window);
+
+SDL.Quit();
