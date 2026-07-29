86ee8f430da16a8ded60c550eb50b06ddc6cc464
Jacob Foshee <jacobf@gmail.com>
2026-07-29 15:41:25 -0500

    feat(maze): new maze level when player finds goal

---
+++ Wrecs.Game.Maze/MazeLevel.cs
@@ -0,0 +1,137 @@
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using SDL3;
+using Wrecs.Systems;
+
+namespace Wrecs.Game.Maze;
+
+class MazeLevel
+{
+    private const float PlayerSize = 10;
+    private const float PlayerSpeed = 10;
+    private const float PlayerSprintMultiplier = 8;
+    private static readonly Vector2 PlayerStart = new(1, 1);
+    private const float MazeScale = 40;
+    private const int MazeCells = 4;
+    private const float MazeSize = MazeCells * MazeScale;
+    private const float GoalSize = 30f;
+
+    public const int WindowPixels = (int)MazeSize + 1;
+
+    private readonly Sim _sim;
+    private readonly ScaledMaze _maze;
+    private readonly StrongBox<bool> _isGameEnded = new(false);
+    private readonly PlayerAgent _player;
+    private readonly GoalEntity _goal;
+    private readonly ulong _frequency;
+    private readonly double _tickInterval;
+
+    private ulong _startCounter;
+    private ulong _lastTickCounter;
+
+    public bool IsGameEnded => _isGameEnded.Value;
+
+    public MazeLevel()
+    {
+        var baseMaze = MazeGenerator.Generate(MazeCells, MazeCells);
+        _maze = new ScaledMaze(baseMaze, MazeScale);
+
+        _sim = new Sim();
+        _sim.AddSystems(new Spatial2DSystem(),
+                        new GameBoundsConstraint(WindowPixels, WindowPixels, PlayerSize),
+                        new MazeWallsUpdateResolver(_maze.GetWalls()),
+                        new AlignedRectangleSystem(),
+                        new AlignedRectangleCollisionEventSystem(),
+                        new PlayerGoalCollisionHandler(),
+                        new EndGameLatchSystem(_isGameEnded));
+
+        _player = new PlayerAgent(PlayerSpeed, PlayerSprintMultiplier);
+        _goal = new GoalEntity();
+        var goalPosition = _maze.GoalPosition;
+        _sim.InitEntities((_player, [new Spatial2DSnapshot(PlayerStart), new AlignedRectangleSnapshot(new(PlayerStart, PlayerSize, PlayerSize))]),
+                          (_goal, [new Spatial2DSnapshot(goalPosition), new AlignedRectangleSnapshot(new(goalPosition, GoalSize, GoalSize))]));
+
+        _startCounter = SDL.GetPerformanceCounter();
+        _lastTickCounter = _startCounter;
+        _frequency = SDL.GetPerformanceFrequency();
+        _tickInterval = _frequency / 30.0;
+    }
+
+    public bool HandleEvent(SDL.Event e)
+    {
+        var type = (SDL.EventType)e.Type;
+        if (type == SDL.EventType.Quit)
+        {
+            return true;
+        }
+
+        if (type != SDL.EventType.KeyDown && type != SDL.EventType.KeyUp)
+        {
+            return false;
+        }
+
+        switch (e.Key.Key)
+        {
+            case SDL.Keycode.Q:
+            case SDL.Keycode.Escape:
+                return true;
+            case SDL.Keycode.C:
+                _sim.DisableSystem<MazeWallsUpdateResolver>();
+                break;
+            case SDL.Keycode.K:
+                _sim.EnableSystem<MazeWallsUpdateResolver>();
+                break;
+        }
+
+        return false;
+    }
+
+    public void UpdateAndRender(nint renderer)
+    {
+        _player.HandleKeyboard();
+
+        var currentCounter = SDL.GetPerformanceCounter();
+        var elapsed = (currentCounter - _startCounter) / (double)_frequency;
+
+        SDL.SetRenderDrawColor(renderer, 100, 149, 237, 255);
+        SDL.RenderClear(renderer);
+
+        SDL.SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        foreach (var wall in _maze.GetWalls())
+        {
+            SDL.RenderLine(renderer, wall.Start.X, wall.Start.Y, wall.End.X, wall.End.Y);
+        }
+
+        var goalRect = _sim.GetSystem<AlignedRectangleSystem>().GetTypedState(_goal).Rectangle;
+        var sdlGoalRect = new SDL.FRect
+        {
+            X = goalRect.BottomLeft.X,
+            Y = goalRect.BottomLeft.Y,
+            W = goalRect.Width,
+            H = goalRect.Height,
+        };
+        SDL.SetRenderDrawColor(renderer, 255, 215, 0, 255);
+        SDL.RenderFillRect(renderer, in sdlGoalRect);
+
+        var playerRect = _sim.GetSystem<AlignedRectangleSystem>().GetTypedState(_player).Rectangle;
+        var sdlPlayerRect = new SDL.FRect { X = playerRect.BottomLeft.X, Y = playerRect.BottomLeft.Y, W = playerRect.Width, H = playerRect.Height };
+        SDL.SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL.RenderFillRect(renderer, in sdlPlayerRect);
+
+        var playerPosition = _sim.GetSystem<Spatial2DSystem>().GetTypedState(_player).Position;
+        var sdlPlayerPositionRect = new SDL.FRect { X = playerPosition.X - 2, Y = playerPosition.Y - 2, W = 4, H = 4 };
+        SDL.SetRenderDrawColor(renderer, 127, 255, 127, 255);
+        SDL.RenderFillRect(renderer, in sdlPlayerPositionRect);
+
+        SDL.SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL.RenderDebugText(renderer, 10, 10, $"Elapsed Time: {elapsed:F3} seconds");
+
+        SDL.RenderPresent(renderer);
+
+        if (currentCounter - _lastTickCounter >= _tickInterval)
+        {
+            _sim.Tick();
+            _lastTickCounter = currentCounter;
+        }
+    }
+}
--- Wrecs.Game.Maze/Program.cs
+++ Wrecs.Game.Maze/Program.cs
@@ -1,40 +1,6 @@
-﻿using System.Numerics;
-using System.Runtime.CompilerServices;
-using SDL3;
-using Wrecs;
-using Wrecs.Systems;
+﻿using SDL3;
 
 Console.WriteLine("Initializing...");
-
-
-const float PlayerSize = 10;
-const float PlayerSpeed = 10;
-const float PlayerSprintMultiplier = 8;
-Vector2 PlayerStart = new(1, 1);
-const float MazeScale = 40;
-const int MazeCells = 2;
-const float MazeSize = MazeCells * MazeScale;
-const float GoalSize = 30f;
-
-// Create random maze
-var baseMaze = MazeGenerator.Generate(MazeCells, MazeCells);
-var maze = new ScaledMaze(baseMaze, MazeScale);
-
-// Setup Wrecs Sim
-var sim = new Sim();
-var isGameEnded = new StrongBox<bool>(false);
-sim.AddSystems(new Spatial2DSystem(),
-               new GameBoundsConstraint(MazeSize + 1, MazeSize + 1, PlayerSize),
-               new MazeWallsUpdateResolver(maze.GetWalls()),
-               new AlignedRectangleSystem(),
-               new AlignedRectangleCollisionEventSystem(),
-               new PlayerGoalCollisionHandler(),
-               new EndGameLatchSystem(isGameEnded));
-var player = new PlayerAgent(PlayerSpeed, PlayerSprintMultiplier);
-var goal = new GoalEntity();
-var goalPosition = maze.GoalPosition;
-sim.InitEntities((player, [new Spatial2DSnapshot(PlayerStart), new AlignedRectangleSnapshot(new(PlayerStart, PlayerSize, PlayerSize))]),
-                 (goal, [new Spatial2DSnapshot(goalPosition), new AlignedRectangleSnapshot(new(goalPosition, GoalSize, GoalSize))]));
 
 // HACK: Switch to STA Single Threaded Apartment
 #if WINDOWS
@@ -49,98 +15,36 @@ if (!SDL.Init(SDL.InitFlags.Video))
     return;
 }
 
-if (!SDL.CreateWindowAndRenderer("Maze", (int)MazeSize + 1, (int)MazeSize + 1, 0, out var window, out var renderer))
+if (!SDL.CreateWindowAndRenderer("Maze", MazeLevel.WindowPixels, MazeLevel.WindowPixels, 0, out var window, out var renderer))
 {
     SDL.LogError(SDL.LogCategory.Application, $"Error creating window and rendering: {SDL.GetError()}");
     return;
 }
 
+var level = new MazeLevel();
 var playerQuit = false;
-var startCounter = SDL.GetPerformanceCounter();
-var lastTickCounter = startCounter;
-var frequency = SDL.GetPerformanceFrequency();
-var tickInterval = frequency / 30.0;
 
 Console.WriteLine("Let's go!");
 
-while (!playerQuit && !isGameEnded.Value)
+while (!playerQuit)
 {
     while (SDL.PollEvent(out var e))
     {
-        var type = (SDL.EventType)e.Type;
-        if (type == SDL.EventType.Quit)
+        if (level.HandleEvent(e))
         {
             playerQuit = true;
         }
-        else if (type == SDL.EventType.KeyDown || type == SDL.EventType.KeyUp)
-        {
-            switch (e.Key.Key)
-            {
-                case SDL.Keycode.Q:
-                case SDL.Keycode.Escape:
-                    playerQuit = true;
-                    break;
-                case SDL.Keycode.C:
-                    sim.DisableSystem<MazeWallsUpdateResolver>();
-                    break;
-                case SDL.Keycode.K:
-                    sim.EnableSystem<MazeWallsUpdateResolver>();
-                    break;
-            }
-        }
-    }
-    player.HandleKeyboard();
-
-
-    // Calculate elapsed time
-    var currentCounter = SDL.GetPerformanceCounter();
-    var elapsed = (currentCounter - startCounter) / (double)frequency;
-
-    // Clear
-    SDL.SetRenderDrawColor(renderer, 100, 149, 237, 255);
-    SDL.RenderClear(renderer);
-
-    // Draw maze walls
-    SDL.SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    foreach (var wall in maze.GetWalls())
-    {
-        SDL.RenderLine(renderer, wall.Start.X, wall.Start.Y, wall.End.X, wall.End.Y);
     }
 
-    // Draw the goal
-    var goalRect = sim.GetSystem<AlignedRectangleSystem>().GetTypedState(goal).Rectangle;
-    var sdlGoalRect = new SDL.FRect
+    if (playerQuit)
     {
-        X = goalRect.BottomLeft.X,
-        Y = goalRect.BottomLeft.Y,
-        W = goalRect.Width,
-        H = goalRect.Height,
-    };
-    SDL.SetRenderDrawColor(renderer, 255, 215, 0, 255);
-    SDL.RenderFillRect(renderer, in sdlGoalRect);
+        break;
+    }
 
-    // Draw player rect
-    var playerRect = sim.GetSystem<AlignedRectangleSystem>().GetTypedState(player).Rectangle;
-    var sdlPlayerRect = new SDL.FRect { X = playerRect.BottomLeft.X, Y = playerRect.BottomLeft.Y, W = playerRect.Width, H = playerRect.Height };
-    SDL.SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL.RenderFillRect(renderer, in sdlPlayerRect);
-
-    // Draw player position
-    var playerPosition = sim.GetSystem<Spatial2DSystem>().GetTypedState(player).Position;
-    var sdlPlayerPositionRect = new SDL.FRect { X = playerPosition.X - 2, Y = playerPosition.Y - 2, W = 4, H = 4 };
-    SDL.SetRenderDrawColor(renderer, 127, 255, 127, 255);
-    SDL.RenderFillRect(renderer, in sdlPlayerPositionRect);
-
-    // Draw overlay
-    SDL.SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL.RenderDebugText(renderer, 10, 10, $"Elapsed Time: {elapsed:F3} seconds");
-
-    SDL.RenderPresent(renderer);
-
-    if (currentCounter - lastTickCounter >= tickInterval)
+    level.UpdateAndRender(renderer);
+    if (level.IsGameEnded)
     {
-        sim.Tick();
-        lastTickCounter = currentCounter;
+        level = new MazeLevel();
     }
 }
 
